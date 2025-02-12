7e278e2c9827a113d7b2f138f4c2e5c33f91640c
Chalum0 <raphaelzanatta24@proton.me>
2025-02-12 14:57:05 +0100

    [FEAT/XXXXX] Added calculator + test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ export const metadata = {
   title: "App Router",
 };
 
+//import Counter from "app/counter";
+import Calculator from "app/calculator"
+
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <h1>App Router</h1>
+      <Calculator />
+    </>
+    );
 }
